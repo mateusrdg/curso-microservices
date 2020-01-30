c6c79676e571cdcb752d6a1cd9bfb5e4e139264b
mateusrdg <mateusrodrigues-cp@hotmail.com>
2020-01-29 23:20:31 -0300

    primeiro commit microservices

---
+++ src/main/java/mateus/curso/microservicos/curso/CursoApplication.java
@@ -0,0 +1,13 @@
+package mateus.curso.microservicos.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mateus/curso/microservicos/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package mateus.curso.microservicos.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
