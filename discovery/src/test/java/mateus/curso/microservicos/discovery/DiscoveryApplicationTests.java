d71330dbf728474094e9c893441d749634f08a8e
mateusrdg <mateusrodrigues-cp@hotmail.com>
2020-01-30 23:14:08 -0300

    Discovery

---
+++ discovery/src/test/java/mateus/curso/microservicos/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package mateus.curso.microservicos.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
